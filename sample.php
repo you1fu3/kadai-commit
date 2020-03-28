b4798522687b0e74cad6a2181bee1f4c6d69ff26
youfu <youfufu16@gmail.com>
2020-03-28 12:50:40 +0000

    update sample.php

---
+++ sample.php
@@ -0,0 +1,18 @@
+<?php
+    $result = '無し';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
+    </body>
+</html>
